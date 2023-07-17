f614c674a45d109679766504bf2c89ca6b265edd
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-07-17 12:53:39 +0300

    5 - Запрос данных через ст.библиотеку https

--- weather.js
+++ weather.js
@@ -1,16 +1,17 @@
 #!/usr/bin/evt node
 import { getArgs } from "./helpers/args.js"
+import { getWeather } from "./services/api.service.js"
 import { printHelp, printSuccess, printError } from "./services/log.service.js"
-import { saveKeyValue } from "./services/storage.service.js"
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js"
 
 const saveToken = async token => {
   if (!token.length) {
-    printError("Не передан токен!")
+    printError("Не передан token!")
     return
   }
   try {
-    await saveKeyValue("token", token)
-    printSuccess("Токен сохранён")
+    await saveKeyValue(TOKEN_DICTIONARY.token, token)
+    printSuccess("Token сохранён")
   } catch (error) {
     printError(error.message)
   }
@@ -28,6 +29,7 @@ const initCLI = () => {
   if (args.t) {
     return saveToken(args.t)
   }
+  getWeather("москва")
   // вывести погоду
 }
 
